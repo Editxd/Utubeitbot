4deb53f838be729171e36aaa119348ad35e0d795
Hevrji <116875997+Hevrji@users.noreply.github.com>
2023-12-30 12:51:16 +0530

    Update non-auth-user.py

--- bot/plugins/non-auth-user.py
+++ bot/plugins/non-auth-user.py
@@ -10,11 +10,3 @@ from ..config import Config
 log = logging.getLogger(__name__)
 
 
-@UtubeBot.on_message(
-    Filters.private & Filters.incoming 
-)
-async def (c: UtubeBot, m: Message):
-    await m.delete(False)
-    log.info(
-        f"{Config.AUTH_USERS} Unauthorised user {m.chat} contacted. Message {m} deleted!!"
-    )
